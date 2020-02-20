efc024857991d9abe5568f32dfa42315aa1d1966
Darren Neimke <darren.neimke@live.com>
2020-02-20 13:40:46 +1030

    added shell for Todo component

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,2 +1,3 @@
 export * from './navbar';
 export * from './todolist';
+export * from './todo';
---
+++ src/components/todo.tsx
@@ -0,0 +1,10 @@
+import React, { FunctionComponent } from 'react'
+
+interface ITodoProps { }
+
+export const Todo: FunctionComponent<ITodoProps> = ({ }) => {
+
+    return (
+        <div></div>
+    )
+}
